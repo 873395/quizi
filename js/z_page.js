7db6730fa5cf222d6bfa6002359a64c30db7ff79
Kubasiak1 <75041234+Kubasiak1@users.noreply.github.com>
2021-10-21 20:56:20 +0200

    V1
    Główny szablon strony, drobne elementy graficzne

---
+++ js/z_page.js
@@ -0,0 +1,8 @@
+function przenosiny_na_strone(liczba)
+{
+    if(liczba==1){window.open("../Licznik_na_strim/timer.html","_self")}
+    if(liczba==2){window.open("../Licznik_na_strim/stoper.html","_self")}
+    if(liczba==3){window.open("../Licznik_na_strim/index.html","_self")}
+    if(liczba==4){window.open("../Licznik_na_strim/o_nas.html","_self")}
+    if(liczba==5){window.open("../Licznik_na_strim/kontakt.html","_self")}
+}
