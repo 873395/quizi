f87c31ca68910f634aea0981a4ceda7426a6bc7e
Kubasiak1 <75041234+Kubasiak1@users.noreply.github.com>
2021-10-27 16:59:01 +0200

    v2.3
    drobne poprawki

--- js/change_img.js
+++ js/change_img.js
@@ -1,12 +1,20 @@
-function ch_img()
-{
-    let zdj_lista=[
+ let zdj_lista=[
         "https://cdnb.artstation.com/p/assets/images/images/041/782/087/large/adrian-ziajor-6sd.jpg?1632683135",
         "https://cdnb.artstation.com/p/assets/images/images/041/782/083/large/adrian-ziajor-4sd.jpg?1632683122",
         "https://cdna.artstation.com/p/assets/images/images/041/782/078/large/adrian-ziajor-2sd.jpg?1632683109",
     ];
-    var image = document.createElement("img");
-    image.setAttribute("src", zdj_lista[0]);
-    img.appendChild(image);
-    console.warn(zdj_lista[1]);
-}
+    let akt_zdj=0;
+    let a = document.getElementById("c_box_img_1");
+    
+    function czas_wczytaj()
+    {
+        akt_zdj++;
+        if(akt_zdj>=zdj_lista.length)
+        {
+            akt_zdj=0;
+        }
+        a.src=zdj_lista[akt_zdj];
+
+        setTimeout(czas_wczytaj,5000);
+    }
+    
